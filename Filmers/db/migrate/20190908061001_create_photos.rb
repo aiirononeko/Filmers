734a9a0feecdd64bd6d6668853eedd1b23b3c802
Ryota Katada <aiirononeko@gmail.com>
2019-09-08 15:12:42 +0900

    AddPhotosMigration

---
+++ Filmers/db/migrate/20190908061001_create_photos.rb
@@ -0,0 +1,10 @@
+class CreatePhotos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :photos do |t|
+      t.string :title, null: false
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
