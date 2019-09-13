83f4d50d044e8b42e46f9855fc74f15ddb54f478
Ryota Katada <aiirononeko@gmail.com>
2019-09-13 17:11:05 +0900

    BugFixed

--- Filmers/app/controllers/posts_controller.rb
+++ Filmers/app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :description)
   end
 
   def set_post
